4954df18d904109aae6350d5989dee9942af827f
andresslacson1989 <48279587+andresslacson1989@users.noreply.github.com>
2020-02-12 19:12:13 +0800

    Create pptp-add.sh

---
+++ pptp-add.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# created by Cyber
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+ 
+if [ -e "/var/lib/premium-script" ]; then
+		clear
+	else
+		mkdir /var/lib/premium-script;
+fi
+
+username=test
+password=pass
+expiry=1
+today=`date +%s`
+expiry_date=$(( $expiry * 86400 ))
+expiry_day=$(($today + $expiry_date))
+MYIP=$(wget -qO- ipv4.icanhazip.com)
+echo "$username	*	$password	*" >> /etc/ppp/chap-secrets
+echo "$username *   $password   *  $expiry_day"  >> /var/lib/premium-script/data-user-pptp
+fi 
